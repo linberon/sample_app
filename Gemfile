282874bc387e22a773557fdd04e599b23cfca05f
Katalin Beron <linberon@gmail.com>
2019-06-25 15:23:47 +0100

    finish user edit, update, index, and destroy action

--- Gemfile
+++ Gemfile
@@ -3,10 +3,13 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.6'
 
-gem 'bootstrap-sass', '3.3.6'
+gem 'bootstrap-sass', '3.3.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
-gem 'bcrypt', '3.1.11'
+gem 'bcrypt', '3.1.12'
+gem 'faker', '1.7.3'
+gem 'will_paginate', '3.1.7'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -18,7 +21,7 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
-gem 'jquery-rails'
+gem 'jquery-rails', '4.3.1'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,18 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	#Remembers a user in a persistent session
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
 	end
  
+ 	# Returns true if the given user is the current user
+ 	def current_user?(user)
+ 		user == current_user
+ 	end
+
 	def current_user
 		if (user_id = session[:user_id])
 			@current_user ||=User.find_by(id: user_id)
@@ -37,4 +43,16 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
+	#Redirects to stored location or to the default
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	#Stores the URL trying to be accessed
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
+	end
 end
+
